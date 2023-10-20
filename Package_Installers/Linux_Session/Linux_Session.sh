df72f2557e5257a66e6852387aac975860f9456f
systemboys <systemboys@hotmail.com>
2023-10-20 16:31:19 -0300

    Edições inúteis rsrsrsr. [ajuste 1]

--- Package_Installers/Linux_Session/Linux_Session.sh
+++ Package_Installers/Linux_Session/Linux_Session.sh
@@ -21,9 +21,38 @@ sair_do_script() {
     exit "$1"
 }
 
-# Função para atualizar pacotes Linux
+# Função para atualizar pacotes Linux com barra de progresso
 update_packages() {
-    sudo apt-get update
+    (
+        # Inicia a atualização em segundo plano e redireciona a saída para um arquivo temporário
+        sudo apt-get update > /tmp/apt_progress 2>&1 &
+        
+        # Obtém o número total de linhas no arquivo temporário
+        total_lines=$(wc -l < /tmp/apt_progress)
+
+        # Loop para processar o arquivo temporário e atualizar a barra de progresso
+        while true; do
+            # Obtém o número atual de linhas no arquivo temporário
+            current_lines=$(wc -l < /tmp/apt_progress)
+            
+            # Calcula o progresso como uma porcentagem
+            progress=$(( (current_lines * 100) / total_lines ))
+
+            # Atualiza a barra de progresso
+            echo "$progress"
+            
+            # Aguarda 1 segundo antes de verificar novamente
+            sleep 1
+
+            # Sai do loop quando a atualização estiver completa
+            [ "$current_lines" -eq "$total_lines" ] && break
+        done
+    ) | dialog --title "Atualizando Pacotes" --gauge "Aguarde, atualizando pacotes..." 10 70 0
+
+    # Limpa o arquivo temporário
+    rm -f /tmp/apt_progress
+
+    # Exibe a mensagem de conclusão
     dialog --msgbox "Pacotes Linux atualizados!" 8 40
 }
 
