5848caa5b4d780fc013ae008e5cc6c0a51635dfd
systemboys <systemboys@hotmail.com>
2023-10-21 19:09:29 -0300

    Sessão Linux: Senha do usuário root. [ajuste 17]

--- Package_Installers/Linux_Session/Linux_Session.sh
+++ Package_Installers/Linux_Session/Linux_Session.sh
@@ -62,17 +62,17 @@ RootUserPassword() {
     # Captura a senha do usuário usando o dialog
     passRoot=$(dialog --title 'Redefinir senha' --passwordbox 'Por favor, informe a senha:' 0 0 3>&1 1>&2 2>&3)
     # Verifica se o usuário pressionou Cancelar
-    # if [ $? -ne 0 ]; then
-    #     echo "Você cancelou a operação."
-    # else
-    #     echo "Você digitou a senha: $passRoot"
-    # fi
+    if [ $? -ne 0 ]; then
+        echo "Você cancelou a operação."
+    else
+        echo "Você digitou a senha: $passRoot"
+    fi
     # -----------------
     # dialog --inputbox 'Digite a nova senha:' 0 0  2>/tmp/passRoot.txt
-    passRoot=$( cat /tmp/passRoot.txt )
-    cd ../..
-    echo "$passRoot" | sudo passwd root
-    echo "$passRoot" | su -c ./${fileName} root
+    # passRoot=$( cat /tmp/passRoot.txt )
+    # cd ../..
+    # echo "$passRoot" | sudo passwd root
+    # echo "$passRoot" | su -c ./${fileName} root
     # -----------------
 }
 
