97d3afd7ea33b3a120fc245211c2badbabfd8afd
systemboys <systemboys@hotmail.com>
2023-10-27 16:57:07 -0300

    Sessão: Redes. [auste 0.8.1]

--- Package_Installers/Network_Session/Network_Session.sh
+++ Package_Installers/Network_Session/Network_Session.sh
@@ -31,7 +31,7 @@ while true; do
             --menu "${sessionDescription}" 15 40 2 \
             0 "Voltar..." \
             1 "Verificar IP" \
-            2 "Disparar um PING" \
+            2 "Disparar PING" \
             2>&1 >/dev/tty)
 
     # Se o usuário pressionar Cancelar, sair do loop
