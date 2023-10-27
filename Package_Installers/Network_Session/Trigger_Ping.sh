6ecd3447d32d28b069d2a3c0ddc1dc14a205f8f1
systemboys <systemboys@hotmail.com>
2023-10-27 16:55:00 -0300

    Sessão: Redes. [auste 0.7]

--- Package_Installers/Network_Session/Trigger_Ping.sh
+++ Package_Installers/Network_Session/Trigger_Ping.sh
@@ -18,25 +18,48 @@ pingDomain() {
     dialog --title "Resultado do Ping para $domain" --msgbox "$ping_result" 20 100
 }
 
+# Função para realizar o ping forçando IPv4 e mostrar o resultado em uma janela de mensagem
+pingDomainIPv4() {
+    # Solicita ao usuário que insira o domínio usando o dialog
+    domain=$(dialog --inputbox 'Digite o domínio:' 8 40 3>&1 1>&2 2>&3)
+
+    # Verifica se o campo de domínio está vazio
+    if [ -z "$domain" ]; then
+        dialog --msgbox "O domínio não pode estar vazio. Por favor, tente novamente." 8 40
+        return
+    fi
+
+    # Pinga o domínio e armazena o resultado
+    ping_result=$(ping -4 -c 5 "$domain")
+
+    # Exibe o resultado em uma janela de mensagem usando dialog
+    dialog --title "Resultado do Ping para $domain" --msgbox "$ping_result" 20 100
+}
+
 # Inicia o loop para o menu interativo usando dialog
 while true; do
     # Mostra um menu para escolher entre pingar um domínio ou sair
     choice=$(dialog --clear --backtitle "Ping Tool" \
             --menu "Escolha uma opção:" 10 40 2 \
+            0 "Sair..." \
             1 "Pingar um Domínio" \
-            2 "Sair..." \
+            2 "Pingar um Domínio forçando IPv4" \
             2>&1 >/dev/tty)
 
     # Verifica a escolha do usuário
     case "$choice" in
+        0)
+            # Sai do script se o usuário escolher sair
+            clear
+            exit 0
+            ;;
         1)
             # Chama a função para pingar um domínio
             pingDomain
             ;;
-        2)
-            # Sai do script se o usuário escolher sair
-            clear
-            exit 0
+        1)
+            # Chama a função para pingar um domínio forçando IPv4
+            pingDomainIPv4
             ;;
     esac
 done
