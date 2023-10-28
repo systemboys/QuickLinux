37b14424a3bdc743104a20491dad0fe93adeb7fa
systemboys <systemboys@hotmail.com>
2023-10-27 23:44:33 -0300

    Sessão: Redes. [auste 1.1]

--- Package_Installers/Network_Session/Trigger_Ping.sh
+++ Package_Installers/Network_Session/Trigger_Ping.sh
@@ -2,6 +2,8 @@
 
 # Função para realizar o ping e mostrar o resultado em uma janela de mensagem
 pingDomain() {
+    $cmdIP=$1
+
     # Solicita ao usuário que insira o domínio usando o dialog
     domain=$(dialog --inputbox 'Digite o domínio:' 8 40 3>&1 1>&2 2>&3)
 
@@ -12,28 +14,10 @@ pingDomain() {
     fi
 
     # Pinga o domínio e armazena o resultado
-    ping_result=$(ping -c 5 "$domain")
+    ping_result=$(ping "$cmdIP" 5 "$domain")
 
     # Exibe o resultado em uma janela de mensagem usando dialog
-    dialog --title "Resultado do Ping para $domain" --msgbox "$ping_result" 20 100
-}
-
-# Função para realizar o ping forçando IPv4 e mostrar o resultado em uma janela de mensagem
-pingDomainIPv4() {
-    # Solicita ao usuário que insira o domínio usando o dialog
-    domain=$(dialog --inputbox 'Digite o domínio:' 8 40 3>&1 1>&2 2>&3)
-
-    # Verifica se o campo de domínio está vazio
-    if [ -z "$domain" ]; then
-        dialog --msgbox "O domínio não pode estar vazio. Por favor, tente novamente." 8 40
-        return
-    fi
-
-    # Pinga o domínio e armazena o resultado
-    ping_result=$(ping -4 -c 5 "$domain")
-
-    # Exibe o resultado em uma janela de mensagem usando dialog
-    dialog --title "Resultado do Ping para $domain" --msgbox "$ping_result" 20 100
+    dialog --title "Resultado do Ping para $domain => $cmdIP" --msgbox "$ping_result" 20 100
 }
 
 # Inicia o loop para o menu interativo usando dialog
@@ -55,11 +39,13 @@ while true; do
             ;;
         1)
             # Chama a função para pingar um domínio
-            pingDomain
+            cmdIP="-c"
+            pingDomain "$cmdIP"
             ;;
         2)
             # Chama a função para pingar um domínio forçando IPv4
-            pingDomainIPv4
+            cmdIP="-4 -c"
+            pingDomain "$cmdIP"
             ;;
     esac
 done
