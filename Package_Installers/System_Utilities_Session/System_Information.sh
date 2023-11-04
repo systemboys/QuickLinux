c1383f8803f1297a9156e8c6a646fa3fd2bac5f8
systemboys <systemboys@hotmail.com>
2023-11-04 18:19:41 -0300

    Adicionada a opção 'Utilitários do Sistema'. [0.1.6]

--- Package_Installers/System_Utilities_Session/System_Information.sh
+++ Package_Installers/System_Utilities_Session/System_Information.sh
@@ -23,8 +23,5 @@ processor_info=$(grep "model name" /proc/cpuinfo | head -n 1 | cut -d ":" -f 2 |
 memory_info=$(grep "MemTotal" /proc/meminfo | cut -d ":" -f 2 | sed 's/^[ \t]*//')
 
 # Imprimir as informações obtidas
-dialog --msgbox \
-    "Informações do processador: $processor_info" \
-    "Informações da memória: $memory_info" \
-    8 40
+dialog --msgbox "Informações do processador: $processor_info\nInformações da memória: $memory_info" 8 40
 
