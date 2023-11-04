ce274f70b6368c682c5e525ba8780e57adaa7799
systemboys <systemboys@hotmail.com>
2023-11-04 18:13:18 -0300

    Adicionada a opção 'Utilitários do Sistema'. [0.1.2]

--- Package_Installers/System_Utilities_Session/System_Information.sh
+++ Package_Installers/System_Utilities_Session/System_Information.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# System_Information.sh - Obter "Informações do processador e memória".
+#
+# Autor: Marcos Aurélio R. da Silva <systemboys@hotmail.com>
+# Manutenção: Marcos Aurélio R. da Silva <systemboys@hotmail.com>
+#
+# ---------------------------------------------------------------
+# Este programa tem a finalidade de obter informações do processador e memória.
+# ---------------------------------------------------------------
+# Histórico:
+# v0.0.1 2023-11-04 às 17h50, Marcos Aurélio:
+#   - Versão inicial, obter informações do processador e memória.
+#
+# Licença: GPL.
+
 clear
 
 # Your commands here...
