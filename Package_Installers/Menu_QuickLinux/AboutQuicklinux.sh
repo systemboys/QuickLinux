0a8c24f3b5aacaafae0d26e120bcbfcbc20e667b
systemboys <systemboys@hotmail.com>
2023-11-06 22:57:58 -0300

    Sobre o QuickLinux. [0.0.3]

--- Package_Installers/Menu_QuickLinux/AboutQuicklinux.sh
+++ Package_Installers/Menu_QuickLinux/AboutQuicklinux.sh
@@ -3,9 +3,17 @@
 clear
 
 # Obtém o caminho do arquivo que contém o histórico
-historico_file="../../QuickLinux.sh"
+fileHistory="../../QuickLinux.sh"
 
 # Obtém o número da última versão do histórico do script
-lastVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "$historico_file" | tail -n 1)
+lastVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "$fileHistory" | tail -n 1)
 
-dialog --msgbox "A última versão registrada no script é: $lastVersion" 8 40
+dialog --msgbox "Sobre o QuickLinux
+
+QuickLinux: $lastVersion
+
+Bem-vindo ao QuickLinux, sua ferramenta interativa definitiva para simplificar sua jornada no mundo do Linux. Projetado para tornar as operações diárias no Linux rápidas, fáceis e acessíveis, o QuickLinux é seu parceiro confiável para tarefas essenciais no terminal.
+
+O QuickLinux foi projetado para fazer com que você se sinta em casa no Linux, mesmo se você for um iniciante ou um usuário experiente. Simplifique sua experiência no Linux, reduzindo o tempo gasto em comandos complexos e focando no que realmente importa: seu trabalho, seu projeto e sua criatividade. Experimente a facilidade do QuickLinux agora mesmo!
+
+O QuickLinux é desenvolvido pela GLOBAL TEC Informática, uma empresa que trabalha no ramo de informática desde 2008." 8 40
