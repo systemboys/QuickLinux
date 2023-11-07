2ac22bc4e5172da2eb82a7a99e94ff2c80679082
systemboys <systemboys@hotmail.com>
2023-11-07 00:55:07 -0300

    Testando comparação de verão online com local. [0.0.5]

--- Package_Installers/Menu_QuickLinux/AboutQuickLinux.sh
+++ Package_Installers/Menu_QuickLinux/AboutQuickLinux.sh
@@ -16,31 +16,36 @@
 
 clear
 
-# Obtém o número da última versão do histórico do script
+# Obtém o número da última versão do histórico do script local
 lastLocalVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "../../QuickLinux.sh" | tail -n 1)
 
+# Obtém o número da última versão do histórico do script no GitHub
 lastGitHubVersion=$(curl -s https://github.com/systemboys/QuickLinux/raw/main/QuickLinux.sh | grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | tail -n 1)
 
-dialog --msgbox "Sobre o QuickLinux
+# Exibe as versões em uma caixa de mensagem usando dialog
+dialog --msgbox "Versão local: $lastLocalVersion | Versão no GitHub: $lastGitHubVersion" 10 60
 
-**Sobre o QuickLinux: $lastLocalVersion >>> $lastGitHubVersion <<<**
 
-Bem-vindo ao QuickLinux, sua ferramenta interativa definitiva para simplificar sua jornada no mundo do Linux. Projetado para tornar as operações diárias no Linux rápidas, fáceis e acessíveis, o QuickLinux é seu parceiro confiável para tarefas essenciais no terminal.
+# dialog --msgbox "Sobre o QuickLinux
 
-**O Que o QuickLinux Oferece:**
+# **Sobre o QuickLinux: $lastLocalVersion >>> $lastGitHubVersion <<<**
 
-- **Atualização e Manutenção Simples:** Atualize seus pacotes com um clique. Mantenha seu sistema funcionando sem complicações.
-  
-- **Navegação Rápida:** Instale navegadores populares como Google Chrome, Mozilla Firefox e outros com facilidade para uma experiência de navegação suave.
-  
-- **Ferramentas de Desenvolvimento:** Implemente rapidamente ambientes de desenvolvimento com Docker e Docker Compose. Ganhe eficiência em seu fluxo de trabalho de desenvolvimento.
-  
-- **Controle de Rede:** Verifique seu IP, faça testes de ping e trace rotas para diagnosticar problemas de rede sem complicações.
-  
-- **Exploração do Sistema:** Obtenha informações detalhadas sobre seu processador e memória, compreendendo melhor o funcionamento interno do seu sistema.
-  
-- **Facilidade em um Cenário Terminal:** Execute comandos complexos com um clique, economizando seu tempo e esforço.
-  
-- **Flexibilidade e Personalização:** Selecione e personalize suas instalações de acordo com suas necessidades específicas.
+# Bem-vindo ao QuickLinux, sua ferramenta interativa definitiva para simplificar sua jornada no mundo do Linux. Projetado para tornar as operações diárias no Linux rápidas, fáceis e acessíveis, o QuickLinux é seu parceiro confiável para tarefas essenciais no terminal.
 
-O QuickLinux foi projetado para fazer com que você se sinta em casa no Linux, mesmo se você for um iniciante ou um usuário experiente. Simplifique sua experiência no Linux, reduzindo o tempo gasto em comandos complexos e focando no que realmente importa: seu trabalho, seu projeto e sua criatividade. Experimente a facilidade do QuickLinux agora mesmo!" 16 80
+# **O Que o QuickLinux Oferece:**
+
+# - **Atualização e Manutenção Simples:** Atualize seus pacotes com um clique. Mantenha seu sistema funcionando sem complicações.
+  
+# - **Navegação Rápida:** Instale navegadores populares como Google Chrome, Mozilla Firefox e outros com facilidade para uma experiência de navegação suave.
+  
+# - **Ferramentas de Desenvolvimento:** Implemente rapidamente ambientes de desenvolvimento com Docker e Docker Compose. Ganhe eficiência em seu fluxo de trabalho de desenvolvimento.
+  
+# - **Controle de Rede:** Verifique seu IP, faça testes de ping e trace rotas para diagnosticar problemas de rede sem complicações.
+  
+# - **Exploração do Sistema:** Obtenha informações detalhadas sobre seu processador e memória, compreendendo melhor o funcionamento interno do seu sistema.
+  
+# - **Facilidade em um Cenário Terminal:** Execute comandos complexos com um clique, economizando seu tempo e esforço.
+  
+# - **Flexibilidade e Personalização:** Selecione e personalize suas instalações de acordo com suas necessidades específicas.
+
+# O QuickLinux foi projetado para fazer com que você se sinta em casa no Linux, mesmo se você for um iniciante ou um usuário experiente. Simplifique sua experiência no Linux, reduzindo o tempo gasto em comandos complexos e focando no que realmente importa: seu trabalho, seu projeto e sua criatividade. Experimente a facilidade do QuickLinux agora mesmo!" 16 80
