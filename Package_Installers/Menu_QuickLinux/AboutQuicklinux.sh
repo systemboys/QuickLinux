707064e9d51d3206b80dd05294d00325e83d78bb
systemboys <systemboys@hotmail.com>
2023-11-06 22:47:20 -0300

    Sobre o QuickLinux. [0.0.2]

--- Package_Installers/Menu_QuickLinux/AboutQuicklinux.sh
+++ Package_Installers/Menu_QuickLinux/AboutQuicklinux.sh
@@ -2,6 +2,10 @@
 
 clear
 
-# Your commands here...
+# Obtém o caminho do arquivo que contém o histórico
+historico_file="../../QuickLinux.sh"
 
-dialog --msgbox "Seu comando A foi executado!" 8 40
+# Obtém o número da última versão do histórico do script
+lastVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "$historico_file" | tail -n 1)
+
+dialog --msgbox "A última versão registrada no script é: $lastVersion" 8 40
