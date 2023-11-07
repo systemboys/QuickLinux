7af17fbfd8b2667eba2e837e81d12ef76d3b3e0f
systemboys <systemboys@hotmail.com>
2023-11-06 23:59:31 -0300

    Variáveis globais. [0.0.9]

--- Package_Installers/Menu_QuickLinux/Menu_QuickLinux.sh
+++ Package_Installers/Menu_QuickLinux/Menu_QuickLinux.sh
@@ -34,7 +34,7 @@ fileName="$1"
 developer="$2"
 
 # Variáveis úteis
-sessionName="${programName} ${lastVersion} 🚀🐧"
+sessionName="${programName} ${latestQuicklinksVersion} 🚀🐧"
 sessionDescription="Opções de referência ao QuickMenu."
 
 # Função para atualizar QuickLinux local pelo QuickLinux do repositório
