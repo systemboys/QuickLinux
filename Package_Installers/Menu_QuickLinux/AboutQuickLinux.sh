6511f3be20b1ef0e195d084492e44132d1e3c689
systemboys <systemboys@hotmail.com>
2023-11-07 00:38:38 -0300

    Testando comparação de verão online com local. [0.0.1]

--- Package_Installers/Menu_QuickLinux/AboutQuickLinux.sh
+++ Package_Installers/Menu_QuickLinux/AboutQuickLinux.sh
@@ -16,12 +16,15 @@
 
 clear
 
-# Obtém o número da última versão do histórico do script
-lastVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "../../QuickLinux.sh" | tail -n 1)
+# Obtém o número da última versão do histórico do script local
+latestLocalVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "../../QuickLinux.sh" | tail -n 1)
+
+# Obtém o número da última versão do histórico do script no GitHub
+latestDithubVersion=$(grep -o 'v[0-9]\+\.[0-9]\+\.[0-9]\+' "$(curl -fsSL https://github.com/systemboys/QuickLinux/raw/main/QuickLinux.sh)" | tail -n 1)
 
 dialog --msgbox "Sobre o QuickLinux
 
-**Sobre o QuickLinux: $lastVersion**
+**Sobre o QuickLinux: $latestDithubVersion**
 
 Bem-vindo ao QuickLinux, sua ferramenta interativa definitiva para simplificar sua jornada no mundo do Linux. Projetado para tornar as operações diárias no Linux rápidas, fáceis e acessíveis, o QuickLinux é seu parceiro confiável para tarefas essenciais no terminal.
 
