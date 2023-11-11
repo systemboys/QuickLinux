5aa257b2ac41ca7322ff0041573a4ba9184aaae9
systemboys <systemboys@hotmail.com>
2023-11-11 00:41:53 -0300

    Configuração de IP e interfaces na sessão 'Redes'. [0.0.2]

--- Package_Installers/Network_Session/IPConfigurationAndInterfaces.sh
+++ Package_Installers/Network_Session/IPConfigurationAndInterfaces.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# IPConfigurationAndInterfaces.sh - Executa o script com opções referentes à
+# configurações e interfaces de rede.
+#
+# URL: https://github.com/github_user/project_on_github.git
+# Autor: Marcos Aurélio R. da Silva <systemboys@hotmail.com>
+# Manutenção: Marcos Aurélio R. da Silva <systemboys@hotmail.com>
+#
+# ---------------------------------------------------------------
+# Este programa tem a finalidade de executar o script com opções referentes à
+# configurações e interfaces de rede.
+# ---------------------------------------------------------------
+# Histórico:
+# v0.0.1 2023-11-11 às 00h30, Marcos Aurélio:
+#   - Versão inicial, opções referentes à configurações e interfaces de rede.
+#
+# Licença: GPL.
+
 # Função para realizar o ping e mostrar o resultado em uma janela de mensagem
 pingDomain() {
     # Solicita ao usuário que insira o domínio usando o dialog
@@ -23,20 +40,20 @@ while true; do
     # Mostra um menu para escolher entre pingar um domínio ou sair
     choice=$(dialog --clear --backtitle "Ping Tool" \
             --menu "Escolha uma opção:" 10 40 2 \
+            0 "Voltar..." \
             1 "Pingar um Domínio" \
-            2 "Sair" \
             2>&1 >/dev/tty)
 
     # Verifica a escolha do usuário
     case "$choice" in
-        1)
-            # Chama a função para pingar um domínio
-            pingDomain
-            ;;
-        2)
+        0)
             # Sai do script se o usuário escolher sair
             clear
             exit 0
+            ;;
+        1)
+            # Chama a função para pingar um domínio
+            pingDomain
             ;;
     esac
 done
